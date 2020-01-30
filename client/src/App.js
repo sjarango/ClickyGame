299c910e7ee7bd8fb25c5ee50d4f7c45f3812dd0
Sebastian <sjarango92@gmail.com>
2020-01-30 18:06:54 -0500

    images in thumbnails and shuffles when clicked

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,53 @@ import React, { Component } from "react";
 import Thumbnail from './components/Thumbnail'
 
 class App extends Component {
+
+  state = {
+    tiles: [
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Beth_Smith.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Eyepatchevilmorty.jpg",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Jerry_Smith.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Jessica.png",
+
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Meeseeks.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Morty.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/mr_poopybutthole.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Mr._Goldenfold.png",
+
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Rick.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Talkingcat.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Tammy_and_Bird_Person.png",
+    "https://raw.githubusercontent.com/sjarango/ClickyGame/2c23822c17bcaabca79c2c7ff916e9a1a5d4dd1c/client/images/Tammy.png"
+
+    ]
+  };
+  
+
+  shuffled = [];
+
+  shuffleTiles = () => {
+
+  }
+  
+  handleTitleClick = (evt) => {
+    // record click
+    // test for match
+    console.log(evt);
+    const shuffled = this.state.tiles.sort(() => 0.5 - Math.random());
+
+    this.setState({tiles: shuffled});
+
+  }
+
+  // const newShuffle = this.state.tiles.sort(() => 0.5 - Math.random());
+
   render() {
     return (
       <div className="App">
-        <Thumbnail/>
+        {this.state.tiles.map((tile, idx) => <Thumbnail
+          src={tile} key={idx} onClick={this.handleTitleClick}
+        />)}
+
       </div>
     );
   }
--- client/src/components/Thumbnail.jsx
+++ client/src/components/Thumbnail.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Thumbnail(props) {
   return (
-    <img src={props.src} onClick={props.handleTileClick} className="img-thumbnail" />
+    <img src={props.src} onClick={props.onClick} className="img-thumbnail" />
   );
 } 
